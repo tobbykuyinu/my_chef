0069e91fe18fc19a27ea42184b7434a1de763cda
tobby <tobiloba.kuyinu@carmudi.com>
2017-02-03 14:00:23 +0100

    added metadata

---
+++ node-docker/metadata.rb
@@ -0,0 +1,8 @@
+name        "node-docker"
+description 'Installs and configures a Node.js docker application server'
+maintainer  "AWS OpsWorks"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'deploy'
+depends 'opsworks_commons'
